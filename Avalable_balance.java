78f73e1d89d0e2f1885d9e6206f989bbc7d16ede
Kamohelo01 <92779048+Kamohelo01@users.noreply.github.com>
2021-10-20 16:15:56 +0200

    Added Welcoming Screen V1
    This link https://www.youtube.com/watch?v=toV9LM3xE1I  is for horizontal button

---
+++ Avalable_balance.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Avalable_balance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avalable_balance);
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+
+    }
+}
