066d4303c3711ec212585e13e13d55b3e8bb362b
Lee-Yandre <92723046+Lee-Yandre@users.noreply.github.com>
2021-10-20 16:50:45 +0200

    Add files via upload
    20-Oct
    Greeting Page:
    1.Login(MainActivity)
    2.Sign Up
    3.Home
    
    1.Login 
    User credential fields -Username, Password
    Login button-direct users to homepage
    Number of attempts displayed
    A redirect for Account holders who are new to the app. Redirects user to Sign Up page ,where a new user account can be created.
    
    2.Sign Up
    User credentials:
    Account Number
    New Username
    Password and password confirmation (rather then requiring user to use their ATM/Banking pin)
    
    Sign Up button directs users to home Page
    
    In the event that user do have an account and would to go back to login page "Already have an Account?" takes user back to login page
    
    3.Home Page
    Text displayed "welcome_to_the_mindworx_banking_app"

--- MainActivity.java
+++ MainActivity.java
@@ -1,30 +1,95 @@
-package com.example.myapplication;
-
-import androidx.appcompat.app.AppCompatActivity;
+ package com.example.demo;
 
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private Button button1;
+ public class MainActivity extends AppCompatActivity {
+
+    private EditText eName;
+    private  EditText ePassword;
+    private Button eLogin;
+    private TextView eAttemptsInfo;
+
+    private final String Username="Admin";
+     private  final String Password="12345678";
+
+     boolean isValid =false;
+
+     private int counter =5;
+     TextView CreateNewAccount;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
+        eName= findViewById(R.id.etName);
+        ePassword=findViewById(R.id.etPassword);
+        eLogin=findViewById(R.id.btnLogin);
+        eAttemptsInfo=findViewById(R.id.tvAttemptsinfo);
+        CreateNewAccount=findViewById(R.id.CreateNewAccount);
+
+        CreateNewAccount.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                openAcivity2();
+                startActivity(new Intent(MainActivity.this,SignUpActivity.class));
+
+            }
+        });
+
+        eLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                String inputName=eName.getText().toString();
+                String inputPassword=ePassword.getText().toString();
+
+                if(inputName.isEmpty()|| inputPassword.isEmpty())
+                {
+                            Toast.makeText( MainActivity.this , "Please enter all the details correctly!",Toast.LENGTH_SHORT).show();
+            }else{
+                    isValid =validate(inputName,inputPassword);
+                    if(!isValid){
+                        counter--;
+                        Toast.makeText( MainActivity.this , "Incorrect credentials entered!",Toast.LENGTH_SHORT).show();
+
+                        eAttemptsInfo.setText("No. of attempts remaining: " + counter);
+
+                        if(counter ==0) {
+                            eLogin.setEnabled(false);
+                        }
+
+                    }else{
+                        Toast.makeText( MainActivity.this , "Login successful!",Toast.LENGTH_SHORT).show();
+                        //Add the code to go to new Activity
+                        Intent intent= new Intent(MainActivity.this,HomePageActivity.class);
+                        startActivity(intent);
+                    }
+
+
+
+                }
+
             }
         });
     }
 
-    private void openAcivity2() {
-        Intent intent = new Intent(this, Activity2.class);
-        startActivity(intent);
+    private boolean validate(String name,String password){
+
+        if (name.equals(Username) && password.equals(Password)) {
+            return true;
+        }
+        return false;
     }
 }
