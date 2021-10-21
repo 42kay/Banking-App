6a4056b090dab3a29c218c034a8a2e381a82c105
Kamohelo01 <92779048+Kamohelo01@users.noreply.github.com>
2021-10-21 15:40:29 +0200

    Updated code

---
+++ Buy_Airtime.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Buy_Airtime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_airtime);
+    }
+}
--- Savings_Account.java
+++ Savings_Account.java
@@ -7,35 +7,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Savings_Account extends AppCompatActivity {
     private Button button1;
     private Button button2;
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_2);
 
-        button1 = findViewById(R.id.Savingsaccount);
-        button2 = findViewById(R.id.Chequeaccount);
+        button1 = findViewById(R.id.Transactionhistory);
+        button2 = findViewById(R.id.Accountoptions);
+        button3 = findViewById(R.id.back);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Savings_Account.class);
+                Intent intent = new Intent(Savings_Account.this, Savings_Account.class);
                 startActivity(intent);
             }
         });
-
 
 
         button2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Cheque_Account.class);
+                Intent intent = new Intent(Savings_Account.this, Account_Options.class);
                 startActivity(intent);
             }
         });
 
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Savings_Account.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
-
