74b4d6609077fb32036bfc5c3afa22add368984a
42kay <58327111+42kay@users.noreply.github.com>
2021-10-29 09:33:00 +0200

    Updated Debit_Order.java

--- Debit_Order.java
+++ Debit_Order.java
@@ -1,14 +1,25 @@
-package com.example.myapplication;
+package com.example.demo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Debit_Order extends AppCompatActivity {
-
+    private Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debit_order);
+        button1 = findViewById(R.id.Add_Debit_Order);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Debit_Order.this, Savings_Account.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
